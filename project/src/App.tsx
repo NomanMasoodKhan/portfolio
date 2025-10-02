9877125fd687f0ec9a67459c6eca16fed5c22b36
NomanMasoodKhan <nomanmasoodkhan365@gmail.com>
2025-10-02 22:31:14 +0500

    changed about me

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About';
 import Blog from './components/Blog';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
